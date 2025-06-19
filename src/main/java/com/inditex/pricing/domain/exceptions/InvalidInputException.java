ced02e51e82f4c377c75d782b6bdbbd8711e3d68
Avila-Garrido Miguel-Angel <miguel.avila.1990@gmail.com>
2025-06-19 19:26:54 +0200

    Añadir modelo de dominio

--- src/main/java/com/inditex/pricing/domain/exceptions/InvalidInputException.java
+++ src/main/java/com/inditex/pricing/domain/exceptions/InvalidInputException.java
@@ -1,6 +1,6 @@
 package com.inditex.pricing.domain.exceptions;
 
-public class InvalidInputException extends RuntimeException {
+public class InvalidInputException extends DomainException {
     public InvalidInputException(String message) {
         super(message);
     }
